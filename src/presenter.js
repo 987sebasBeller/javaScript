e0cfc3de4ce9dcc128ed912f490d9322f35c1636
sebastianBeller <sebastianbeller76@gmail.com>
2023-08-10 14:01:01 -0400

    saludo_simple

--- src/presenter.js
+++ src/presenter.js
@@ -1,22 +1,7 @@
-import sumar from "./sumador";
-import multiplicar from "./multiplicador";
-const formSumar = document.querySelector("#sumar-form");
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const div = document.querySelector("#resultado-div");
-const btnMultiplicar=document.getElementById("multiplicar-button");
-formSumar.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-  div.innerHTML = `
-  <h2>Resultado:</h2>  
-  <p>${sumar(firstNumber, secondNumber)} </p>`;
-});
-btnMultiplicar.addEventListener("click", () => {
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-  div.innerHTML = `
-  <h2>Resultado:</h2>  
-  <p>${multiplicar(firstNumber, secondNumber)} </p>`;
+
+const button = document.querySelector("#btn")
+const div=document.querySelector("#saludo")
+button.addEventListener("click", ()=>{
+  var name_input = document.querySelector("#name_input")
+  div.innerHTML=`<p>HOLA ${name_input.value} 😃!!</p>`
 });
