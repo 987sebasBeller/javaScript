6618ea53449cef750b89ea9a8fc6876daa8891a1
sebastianBeller <sebastianbeller76@gmail.com>
2023-08-10 14:22:48 -0400

    saludo_con_nombre

--- src/multiplicador.test.js
+++
@@ -1,7 +0,0 @@
-import multiplicar from "./multiplicador";
-describe("Multiplicar",()=>{
-    it("deberia multiplicar dos numeros", () => {
-        expect(multiplicar(3, 6)).toEqual(18);
-      });
-}
-);
